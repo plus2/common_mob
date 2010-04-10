a3d1a7acf5f8cc5a2e5c9bb9bb7dfd96bce702c3
lachie <lachiec@gmail.com>
2010-04-10 17:56:01 +1000

    change the path used by the default resource locator after refactor

--- lib/common_mob/resource_locator.rb
+++ lib/common_mob/resource_locator.rb
@@ -1,7 +1,7 @@
 module CommonMob
   class ResourceLocator
     def resource(target,name)
-      path = target.ctx.file.to_s.sub(/\.([^\.]+)$/,'')
+      path = target.act.definition_file.to_s.sub(/\.([^\.]+)$/,'')
       (path.pathname + name.to_s)
     end
     alias_method :[], :resource
