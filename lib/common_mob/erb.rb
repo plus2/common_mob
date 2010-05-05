06c57751a3c5231bd09f0b3346603a4433809a20
lachie <lachiec@gmail.com>
2010-05-05 10:43:12 +1000

    taking the ActionView approach, report which line of the template caused an error

--- lib/common_mob/erb.rb
+++ lib/common_mob/erb.rb
@@ -25,12 +25,48 @@ module CommonMob
       end
     end
 
+    class ErbError < StandardError
+      def initialize(file,ex)
+        @file = file
+        @src = file.read
+        @ex  = ex
+      end
+
+      def message
+        "error rendering erb: #{@ex.message}\nat line #{line_number+1} of #{@file}\n#{error_with_context * "\n"}"
+      end
+      def to_s
+        message
+      end
+
+      def error_with_context(ctx=2)
+        start_on_line = [ line_number - ctx - 1, 0          ].max
+        end_on_line   = [ line_number + ctx    , @src.length].min
+
+        @src.split("\n")[ start_on_line..end_on_line ]
+      end
+
+      def line_number
+        @line_number = line_number!
+      end
+      def line_number!
+        re = /\(erb\):(\d+):in `get_binding'/
+        @line_number = $1.to_i if @ex.backtrace.find {|line| line =~ re}
+      end
+
+      def method_missing(meth,*args,&block)
+        @ex.send(meth,*args,&block)
+      end
+    end
+
     def render_erb(src,context)
       e = ERB.new(src.read)
       e.result( Context.new(context || {}).get_binding )
     rescue Object
-      log "error rendering erb: [#{$!.class}] #{$!}"
-      raise $!
+      raise ErbError.new(src, $!)
+      #log "error rendering erb: [#{$!.class}] #{$!}"
+      #log e.src
+      #raise $!
     end
   end
 end
