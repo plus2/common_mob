f7b833cdd0351fdb7f8e3449503a200e56215505
lachie <lachiec@gmail.com>
2010-07-19 16:48:11 +1000

    apt_source target for ... adding apt sources!

--- targets/packages.rb
+++ targets/packages.rb
@@ -43,6 +43,16 @@ class Apt < AngryMob::Target
 
 end
 
+class AptSource < AngryMob::Target
+  include CommonMob::ShellHelper
+  default_action
+  def install
+    act.in_sub_act { apt 'python-software-properties' }
+    # TODO make null op less expensive
+    sh("add-apt-repository #{default_object} && apt-get update").run
+  end
+end
+
 class GemPackage < AngryMob::Target
   nickname 'gem'
 
